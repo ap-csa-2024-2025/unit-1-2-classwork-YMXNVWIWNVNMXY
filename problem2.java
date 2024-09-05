ab252e9bb940d9984e292e4c477a6229d6c7f9d3
YMXNVWIWNVNMXY <jnbui106@student.hbuhsd.edu>
2024-09-05 00:11:10 +0000

    problem2

--- problem2.java
+++ problem2.java
@@ -1,1 +1,13 @@
 // Create your class here; don't forget the main method
+// Create your class here; don't forget the main method
+public class problem2
+{
+  public static void main(String[] args)
+  {
+    String firstName = "David";
+    String secondName = "Elaine";
+    String thirdName = "Jesse";
+    String forthName = "Sandy";
+    System.out.print(forthName + " " + firstName + " " + secondName + " " + thirdName);
+  }
+}
