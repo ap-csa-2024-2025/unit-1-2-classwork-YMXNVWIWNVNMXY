e614247946d1ac1e26f810e404b668febf8c7c27
YMXNVWIWNVNMXY <jnbui106@student.hbuhsd.edu>
2024-09-05 00:14:42 +0000

    problem3

--- problem3.java
+++ problem3.java
@@ -1,1 +1,12 @@
 // Create your class here; don't forget the main method
+// Create your class here; don't forget the main method
+// Create your class here; don't forget the main method
+public class problem3
+{
+  public static void main(String[] args)
+  {
+    String yourName = "Jason";
+    String theStateYouLiveIn = "California";
+    System.out.print("My name is " + yourName + ". I live in " + theStateYouLiveIn + ".");
+  }
+}
